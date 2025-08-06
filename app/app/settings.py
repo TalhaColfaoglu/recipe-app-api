bae7dbf7a112e886959d94b0f1c9d5ce7e773bb8
Talha Colfaoğlu <colfaoglutalha@gmail.com>
2025-08-06 15:34:50 +0300

    fix

--- app/app/settings.py
+++ app/app/settings.py
@@ -25,13 +25,9 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'changeme')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(int(os.environ.get('DEBUG', 0)))
 
-ALLOWED_HOSTS = []
-ALLOWED_HOSTS.extend(
-    filter(
-        None,
-        os.environ.get('DJANGO_ALLOWED_HOSTS', '').split(','),
-    )
-)
+# settings.py
+ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', '').split(',')
+
 
 
 # Application definition
